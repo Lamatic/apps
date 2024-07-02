c5ba3e81075dc20b54eecf29f4e51efcc2032251
amanintech <amanrazdan@amp.dev>
2024-07-01 21:02:47 -0400

    updates notion id

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,8 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'e43926dc27ec4c4895fe222e8e8a2325',
+  //https://lamatic.notion.site/Marketplace-b2665789592e445996b5ad8e88336087?pvs=4
+  rootNotionPageId: 'b2665789592e445996b5ad8e88336087',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
