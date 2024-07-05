1323f4f529ded1ae8e28bd9ab96a416de9324c29
amanintech <amanrazdan@amp.dev>
2024-07-05 17:47:35 -0400

    fixed issues with build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
+//
+// import { NotionPage } from '@/components/NotionPage'
+// import { domain } from '@/lib/config'
+// import { resolveNotionPage } from '@/lib/resolve-notion-page'
 
-import { NotionPage } from '@/components/NotionPage'
-import { domain } from '@/lib/config'
-import { resolveNotionPage } from '@/lib/resolve-notion-page'
-
-export const getStaticProps = async () => {
+/*export const getStaticProps = async () => {
   try {
     const props = await resolveNotionPage(domain)
 
@@ -16,8 +16,8 @@ export const getStaticProps = async () => {
     // let next.js know explicitly that incremental SSG failed
     throw err
   }
-}
+}*/
 
-export default function NotionDomainPage(props) {
+export default function NotionDomainPage() {
   return <h1>Nothing Here</h1>
 }
